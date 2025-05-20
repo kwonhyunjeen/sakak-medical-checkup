5a6d015dffbd4fd1ea891df18f490b53c46d6274
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2025-05-20 16:59:54 +0900

    chore: change eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,19 @@
-import js from "@eslint/js";
+import eslint from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import tailwind from "eslint-plugin-tailwindcss";
+import unicorn from "eslint-plugin-unicorn";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ["dist", "dist-ssr"] },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  unicorn.configs.recommended,
+  ...tailwind.configs["flat/recommended"],
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
+    files: ["**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -20,6 +25,30 @@ export default tseslint.config(
     rules: {
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "unicorn/filename-case": [
+        "error",
+        {
+          cases: {
+            camelCase: true,
+            kebabCase: true,
+            pascalCase: true,
+            snakeCase: false,
+          },
+        },
+      ],
+      "unicorn/prevent-abbreviations": [
+        "error",
+        {
+          replacements: {
+            env: false,
+            envs: false,
+            prop: false,
+            props: false,
+            ref: false,
+            refs: false,
+          },
+        },
+      ],
     },
   },
 );
