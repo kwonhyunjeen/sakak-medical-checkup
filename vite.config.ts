1093b8cf789f1fd3afb910cd9dd52c1337b5384b
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2025-05-21 15:16:58 +0900

    chore: set up vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    checker({
+      overlay: {
+        initialIsOpen: false,
+      },
+      typescript: {
+        tsconfigPath: "./tsconfig.app.json",
+      },
+    }),
+    react(),
+  ],
 });
